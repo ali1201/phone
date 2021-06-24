bb0a427f65cc949038aff85e3ed622e4c7b876c7
Ali <85049617+ali1201@users.noreply.github.com>
2021-06-24 03:45:13 +0300

    Add project files.

---
+++ phone/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace phone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            aphone.START(); 
+            aphone a = new aphone("7,2","10000M", " iphone", "red", "   16G");
+            a.describe();
+            aphone.off();  
+            Console.WriteLine("---------------------------------------------");
+            aphone.START();
+            aphone b = new aphone("5,6","6000m","suamsung","black","2G");
+            b.describe();
+            aphone.off();
+
+
+
+
+        }
+    }
+}
+
---
+++ phone/aphone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class aphone
+{
+    public string display;
+    public string butry;
+    public string model;
+    public string colore;
+    public string Ram;
+    public int number;
+    public aphone(string d, string b , string m , string c , string r)
+    {
+        display = d;
+        butry = b;
+        model = m;
+        colore = c;
+        Ram = r;
+    }
+    public void describe()
+    {
+
+        Console.WriteLine($"phone display  is : {display}");
+        Console.WriteLine($"phone butry is : {butry}");
+        Console.WriteLine($"phone model is : {model}");
+        Console.WriteLine($"phone colore  is : {colore}");
+        Console.WriteLine($"phone Ram is : {Ram}");
+    }
+    public static void START()
+    {
+        Console.WriteLine(" STARTING.........");
+    }
+    public static void off()
+    {
+        Console.WriteLine(" off..........");
+    }
+}
+
